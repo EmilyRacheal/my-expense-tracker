1b2501b6a6af245372f8bcc836f13032bd714d6a
EmilyRacheal <emilychealy4@gmail.com>
2022-06-02 06:18:15 +0100

    added a children props

--- src/components/ExpenseItem/expenseItem.jsx
+++ src/components/ExpenseItem/expenseItem.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "../Card/Card";
 
 const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <h2>{props.title}</h2>
       <div className="expense-item_description">
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
