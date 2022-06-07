36bc29350c00547e096fae1f295abde8d29f5940
EmilyRacheal <emilychealy4@gmail.com>
2022-06-07 11:37:16 +0100

    added newexpense component

---
+++ src/components/NewExpense/newExpense.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../NewExpense/newExpense.css";
+
+function newExpense() {
+  return (
+    <div>
+      <form action=""></form>
+    </div>
+  );
+}
+
+export default newExpense;
